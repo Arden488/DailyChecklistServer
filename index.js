b050185d35b9aea466a83d5625a13358c8f3d037
Anton Samoilov <absamoilov88@gmail.com>
2018-07-15 17:53:04 +0300

    Testing telegram and cron

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const app = express();
 const cors = require('cors');
 const router = require('./router');
 const mongoose = require('mongoose');
+const TelegramBot = require('node-telegram-bot-api');
+const CronJob = require('cron').CronJob;
+
+const bot = new TelegramBot(process.env.TELEGRAM_BOT_API_TOKEN, { polling: true });
+
+new CronJob('00 01 * * * *', function() {
+  bot.sendMessage(37053287, 'It\'s time to fill the checklist, brother!');
+}, null, true, 'Europe/Kiev');
 
 mongoose.connect('mongodb://dcdb_user:A1qKlZuMn6G4@ds239071.mlab.com:39071/daily-checklist');
 
