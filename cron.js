8e2d1f0fe3aa50c3a66afc249fdd749b51ca93bf
Anton Samoilov <absamoilov88@gmail.com>
2018-07-16 23:38:28 +0300

    Change cron time

--- cron.js
+++ cron.js
@@ -4,6 +4,6 @@ const CronJob = require('cron').CronJob;
 
 const bot = new TelegramBot(process.env.TELEGRAM_BOT_API_TOKEN, { polling: true });
 
-new CronJob('00 00 21 * * *', function() {
+new CronJob('00 05 21 * * *', function() {
   bot.sendMessage(37053287, 'It\'s time to fill the checklist, brother!');
 }, null, true, 'Europe/Kiev');
