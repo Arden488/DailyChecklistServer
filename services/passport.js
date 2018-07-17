80b346bb188169a916781e224271611e48845c37
Anton Samoilov <absamoilov88@gmail.com>
2018-07-17 22:00:35 +0300

    Add messages for jwt local strat

--- services/passport.js
+++ services/passport.js
@@ -10,13 +10,13 @@ const localOptions = { usernameField: 'email' };
 const localLogin = new LocalStrategy(localOptions, (email, password, done) => {
     User.findOne({ email: email }, (err, user) => {
         if (err) return done(err, false);
-        if (!user) return done(null, false);
+        if (!user) return done(null, false, { message: 'Incorrect email or password.' });
 
         user.comparePassword(password, function(err, isMatch) {
             if (err) return done(err, false);
-            if (!isMatch) return done(null, false);
+            if (!isMatch) return done(null, false, { message: 'Incorrect email or password.' });
 
-            return done(null, user);
+            return done(null, user, { message: 'Logged In Successfully' });
         });
     });
 });
